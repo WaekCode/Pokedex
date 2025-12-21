2d5c2e9cb2ba75a2bb1fdab820096fc4d5a98c3e
WaekCode <nati275a@gmail.com>
2025-12-21 22:51:01 +0200

    better code structure/more optamized cache

--- location.go
+++ location.go
@@ -26,8 +26,6 @@ type nextBack struct {
 }
 
 func apiLocation(cfg *nextBack, prev bool) (location, error) {
-	
-	
 	var url string
 	if prev {
 		url = cfg.PreviousURL
@@ -38,62 +36,54 @@ func apiLocation(cfg *nextBack, prev bool) (location, error) {
 			url = cfg.NextURL
 		}
 	}
-	c := cfg.Cache
-	res,ok := c.Get(url)
 
-	if ok{
+	c := cfg.Cache
+	res, ok := c.Get(url)
+
+	var result location
+	if ok {
 		fmt.Println("Cache resource being used...")
-		var result location
-		err := json.Unmarshal(res, &result)
+		if err := json.Unmarshal(res, &result); err != nil {
+			return location{}, err
+		}
+	} else {
+		req, err := http.NewRequest("GET", url, nil)
 		if err != nil {
 			return location{}, err
 		}
-		 cfg.NextURL = result.Next
-		if result.Previous == nil {
-			cfg.PreviousURL = ""
-		} else {
-			cfg.PreviousURL = *result.Previous
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			return location{}, err
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			return location{}, fmt.Errorf("API request failed with status: %v", resp.Status)
 		}
 
-		return result, nil
+		respBytes, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return location{}, err
+		}
+
+		// Add to cache
+		c.Add(url, respBytes)
+
+		// Decode for returning
+		if err := json.Unmarshal(respBytes, &result); err != nil {
+			return location{}, err
+		}
 	}
 
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return location{}, err
-	}
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return location{}, err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return location{}, fmt.Errorf("API request failed with status: %v", resp.Status)
-	}
-
-	respBytes, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return location{}, err
-	}
-
-	// Add to cache
-	c.Add(url, respBytes)
-
-	// Decode for returning
-	var result location
-	err = json.Unmarshal(respBytes, &result)
-	if err != nil {
-		return location{}, err
-	}
-
+	// Update next/previous URLs regardless of source (cache or API)
 	cfg.NextURL = result.Next
 	if result.Previous == nil {
 		cfg.PreviousURL = ""
 	} else {
 		cfg.PreviousURL = *result.Previous
 	}
+
 	return result, nil
 }
